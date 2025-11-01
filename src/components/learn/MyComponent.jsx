61e36054e768572581a48606e76e8b925062cab8
AnAnh2k <anducanh125@gmail.com>
2025-11-01 14:02:11 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,5 +1,12 @@
 const MyConponent = () => {
-  return <div>An Đức Anh</div>;
+  return (
+    <>
+      <div>An Đức Anh</div>
+      <div className="child" style={{ borderRadius: "10px", border: "solid" }}>
+        2210A03
+      </div>
+    </>
+  );
 };
 
 export default MyConponent;
