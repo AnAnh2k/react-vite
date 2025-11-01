d830399f0d506815b8b336f249af2d83a2bc6efe
AnAnh2k <anducanh125@gmail.com>
2025-11-01 16:30:22 +0700

    video 33

---
+++ src/components/todo/TodoFooter.jsx
@@ -0,0 +1,10 @@
+const TodoFooter = () => {
+  return (
+    <>
+      <div className="footer">
+        <p>Bản quyền: Đức Anh</p>
+      </div>
+    </>
+  );
+};
+export default TodoFooter;
---
+++ src/components/todo/TodoInput.jsx
@@ -0,0 +1,10 @@
+const TodoInput = () => {
+  return (
+    <div className="todo-input">
+      <input className="input" type="text" placeholder="Enter your task" />
+      <button className="btn">Add</button>
+    </div>
+  );
+};
+
+export default TodoInput;
